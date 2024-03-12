b5b9d6f81377824b2530dc7c39733612e7b2cd09
ramesh kumar mahato <rameshsingh9813@gmail.com>
2024-03-12 21:13:30 +0545

    1st: The blur option is working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+ 
 import './App.css';
+import myImage from './image/imag.jpg';
+import React,{useState} from 'react';
 
 function App() {
+  const styles = [
+    "blur(5px)",
+    "brightness(150%)",
+    "contrast(150%)",
+    "drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.5))",
+    "grayscale(50%)",
+    "hue-rotate(90deg)",
+    "invert(100%)",
+    "opacity(0.8)",
+    "saturate(200%)",
+    "sepia(50%)"
+  ];
+  
+  const imgEffect={
+    filter: styles.join(' ')
+  }
+  const [imageChange,setImageChange]=useState(imgEffect)
+  const [blurs, setBlurs] = useState(0);
+  const [contrast,setContrast]=useState(0);
+
+  const handleBlurness = (event) => {
+    setBlurs(event.target.value);
+    setImageChange(()=>{
+      let blurness=(5/100)*event.target.value
+      return {filter:`blur(${blurness}px)`}
+    })
+  };
+
+  const handleContrast=(e)=>{
+    setContrast(e.target.value);
+    setContrast(()=>{
+      return{filter:`contrast(${e.target.value}%`}
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container ">
+     <img src={myImage} height="400"alt="" style={imageChange}/>
+     <hr />
+    <div className="container">
+      <label for="volume">Blur: </label>
+      <input type="range" id="volume" name="volume" min="0" max="100" value={blurs} onChange={handleBlurness}/>
+         <span id="volumeValue">{blurs}</span>
     </div>
-  );
-}
+    <div className="container">
+      <label for="volume">contrast: </label>
+      <input type="range" id="volume" name="volume" min="0" max="200" value={contrast} onChange={handleContrast}/>
+         <span id="volumeValue"> {contrast}</span>
+    </div>
+      </div>
+      
+    </>
+   );
+ }
 
 export default App;
